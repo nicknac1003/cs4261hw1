3642869447fe3865fe5fe7fbd5da5196eb58e784
nick <42453920+nicknac1003@users.noreply.github.com>
2022-01-29 16:59:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiKey: "AIzaSyDg-ctMzYb58VcFcip2GVT-EpCXXFCqJwg",
-  authDomain: "cs4261hw1-14187.firebaseapp.com",
-  databaseURL: "https://cs4261hw1-14187-default-rtdb.firebaseio.com",
+  apiKey: "{API KEY}",
+  authDomain: "{KEY}",
+  databaseURL: "{KEY}",
 };
 export default config;
